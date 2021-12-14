bbfe8d32df7bec08298d3cfc2be23440b89b2ccf
Ujjwal734 <ujjwalvishwakarma734@gmail.com>
2021-12-14 07:40:26 +0530

    Validating user name

---
+++ User_registration/UserName.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace User_registration
+{
+    public class UserName
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name :- ");
+            string instr = Console.ReadLine();
+            const string name = @"^[A-Z]{1}[a-z]{3,9}$";
+
+            Regex rg = new Regex(name);
+            Console.WriteLine(rg.IsMatch(instr));
+        }
+    }
+}
